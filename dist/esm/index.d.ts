5702ceb21d5b378bba9b4162f4432bd764daa96e
Angelo Raso <angeloraso6@gmail.com>
2025-03-20 14:12:52 -0300

    Version 7.1.0

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { BackgroundModePlugin } from './definitions';
-declare const BackgroundMode: BackgroundModePlugin;
-export * from './definitions';
-export { BackgroundMode };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const BackgroundMode = registerPlugin('BackgroundMode', {
-    web: () => import('./web').then(m => new m.BackgroundModeWeb()),
-});
-export * from './definitions';
-export { BackgroundMode };
-//# sourceMappingURL=index.js.map
